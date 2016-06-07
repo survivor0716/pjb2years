34ff094d5196c85dd9ff2dddf0f6219dc9391ac8
cyn <chenyinan@paymew.com>
2016-06-07 11:23:59 +0800

    做了登录的数据交互

--- src/app/getNow/getNow.controller.js
+++ src/app/getNow/getNow.controller.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
   'use strict';
 
   angular
@@ -6,13 +6,31 @@
     .controller('GetNowController', GetNowController);
 
   /** @ngInject */
-  function GetNowController($log, $scope, api) {
-    //调用登录接口
-    api.login()
-      .then(function (data) {
-        $log.debug(data);
-      }, function (errMsg) {
-        $log.debug(errMsg);
-      });
+  function GetNowController($log, $scope, api, $window,$location) {
+    $scope.Nowshow = false;
+    $scope.isRegister = false;
+    $scope.login = function () {
+      var reg = /^(1[3,4,5,6,7,8]{1})+\d{9}$/;
+      if (reg.test($scope.phoneNum)) {
+        var params = {
+          phone: $scope.phoneNum
+        };
+        //调用登录接口
+        api.login(params)
+          .then(function (data) {
+            $log.debug(data);
+            if (data.register == false) {
+              $scope.Nowshow = true;
+            }
+          }, function (errMsg) {
+            $log.debug(errMsg);
+          });
+      } else {
+        $window.alert("请输入正确的手机号");
+      }
+    }
+    $scope.goToSuccess = function(){
+      $location.path("/register");
+    }
   }
 })();
--- src/app/register/register.js
+++ src/app/register/register.js
@@ -9,7 +9,26 @@
     .controller('RegisterController', RegisterController);
 
   /** @ngInject */
-  function RegisterController($scope, $window, $log, $timeout) {
+  function RegisterController($scope, $window, $log, $timeout,$location) {
+    $scope.showCaptcha=false;
+    $scope.regis=function(){
+      if($scope.pwd == null){
+        $window.alert("请输入密码");
+      }
+      if($scope.pwda != $scope.pwd){
+        $window.alert("两次密码必须一致");
+      }else{
+        api.register(params)
+          .then(function (data) {
+            $log.debug(data);
+            //if (data.register == false) {
+            //  $scope.Nowshow = true;
+            //}
+          }, function (errMsg) {
+            $log.debug(errMsg);
+          });
+      }
+    }
 
   }
 })();
