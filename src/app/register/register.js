c90192497f88f5bf9e10884eb6e20510c892bf08
cyn <chenyinan@paymew.com>
2016-06-07 14:09:05 +0800

    register页的js

--- src/app/register/register.js
+++ src/app/register/register.js
@@ -9,8 +9,9 @@
     .controller('RegisterController', RegisterController);
 
   /** @ngInject */
-  function RegisterController($scope, $window, $log, $timeout,$location) {
+  function RegisterController($scope, $window, $log, $timeout,$location,api) {
     $scope.showCaptcha=false;
+    $scope.captcha="http://piaojubao.h5.dev.willar.net/IdentifyingPicture?random=";
     $scope.regis=function(){
       if($scope.pwd == null){
         $window.alert("请输入密码");
@@ -18,17 +19,46 @@
       if($scope.pwda != $scope.pwd){
         $window.alert("两次密码必须一致");
       }else{
-        api.register(params)
-          .then(function (data) {
-            $log.debug(data);
-            //if (data.register == false) {
-            //  $scope.Nowshow = true;
-            //}
-          }, function (errMsg) {
-            $log.debug(errMsg);
-          });
+        $scope.showCaptcha=true;
       }
     }
-
+    $scope.phoneCode=function(){
+      var Verification={
+        phone:$scope.user.phone
+      }
+      api.verification(Verification)
+        .then(function (data) {
+          $log.debug(data);
+          //if (data.register == false) {
+          //  $scope.Nowshow = true;
+          //}
+        }, function (errMsg) {
+          $log.debug(errMsg);
+          $window.alert(errMsg);
+        });
+    }
+    $scope.reimg=function(){
+      $scope.captcha="http://piaojubao.h5.dev.willar.net/IdentifyingPicture?random="+Math.random();;
+    }
+    $scope.sub=function(){
+      var sub_data = {
+        phone:$scope.user.phone,
+        verification:$scope.verification,
+        password: $scope.pwd,
+        Identifying:$scope.Identifying,
+        parameter:null
+      };
+      $log.debug(sub_data);
+      api.register(sub_data)
+        .then(function (data) {
+          $log.debug(data);
+          //if (data.register == false) {
+          //  $scope.Nowshow = true;
+          //}
+        }, function (errMsg) {
+          $log.debug(errMsg);
+          //$window.alert(errMsg);
+        });
+    }
   }
 })();
