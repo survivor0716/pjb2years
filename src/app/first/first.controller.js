39758ce43d18251da8e0a40902f4735bbd315c15
cyn <chenyinan@paymew.com>
2016-06-12 14:21:35 +0800

    first增加动画效果

--- src/app/first/first.controller.js
+++ src/app/first/first.controller.js
@@ -11,6 +11,36 @@
   /** @ngInject */
   function FirstController($scope, $window, $log, $timeout,$location) {
 
+    //$scope.arrGetImg = [];
+    //
+    //for (var i = 1; i <= 8; i++) {
+    //
+    //  var obj = {
+    //    src: "assets/getImg/" + i + "@2x.png",
+    //    class: "img" + i + " animated",
+    //    random: randomAnimate(),
+    //    active: false
+    //  };
+    //
+    //
+    //  $scope.arrImg.push(obj);
+    //
+    //  (function (obj) {
+    //    $timeout(function () {
+    //      obj.active = true;
+    //      //$log.debug(obj);
+    //      //$log.debug('active = true');
+    //    }, i * 500);
+    //  })(obj);
+    //
+    //}
+    //
+    //$timeout(function () {
+    //  $scope.logoActive = true;
+    //  //$log.debug(obj);
+    //  //$log.debug('active = true');
+    //}, 24 * 500);
+
     $scope.tosec=function(){
       $location.path("/home");
     }
